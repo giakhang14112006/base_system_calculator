37f969fc762c3e5df671aa066dae4c4c7fee76cb
VuChiTon <okazaki36948@gmail.com>
2024-11-06 23:21:15 +0700

    Tun nà

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello DLP");
         System.out.println("Tôn code xong oòi nha");
-        System.out.println("Tôn bị đuổi");
+        System.out.println("Lão Tôn trở lại");
          System.out.println("Tôn dc tuyen");
     }
 }
