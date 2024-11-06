1625a015c3012ce7b1ad7312038afa049ab06e8e
Đặng Tuấnn <datha.tuann79@gmail.com>
2024-11-06 15:48:28 +0700

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello DLP");
         System.out.println("Tôn code xong oòi nha");
-
+        System.out.println("Tôn bị đuổi");
     }
 }
